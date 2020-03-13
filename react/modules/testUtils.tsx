03e45c8940704f0974bc708fa040aa0f156bdaa6
victorhmp <victor2142@gmail.com>
2020-03-13 16:05:45 -0300

    Move testUtils to modules directory

---
+++ react/modules/testUtils.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement } from 'react'
+import { render } from '@vtex/test-tools/react'
+
+import { ProductContextProvider } from '../__mocks__/vtex.product-context/ProductContextProvider'
+
+export function renderWithProductContext(Component: ReactElement, value: any) {
+  return render(
+    <ProductContextProvider value={value}>{Component}</ProductContextProvider>
+  )
+}
+
+export function findCSSHandles(container: HTMLElement, handles: string[]) {
+  const foundNodes = handles
+    .map(handle => {
+      const foundNodesInner = container.getElementsByClassName(handle)
+      return foundNodesInner.length > 0 ? handle : ''
+    })
+    .filter(result => result !== '')
+
+  return foundNodes
+}
