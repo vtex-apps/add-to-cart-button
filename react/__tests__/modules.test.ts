5d2bbe628483928d0d0653da3b7c0be5302712f3
Breno Calazans <brenocalazans@gmail.com>
2020-07-23 14:43:37 -0300

    Update tests

--- react/__tests__/modules.test.ts
+++ react/__tests__/modules.test.ts
@@ -79,19 +79,18 @@ describe('assemblyOptions module', () => {
   })
 
   describe('transformAssemblyOptions function', () => {
-    it('should correctly transform full assembly options', () => {
+    it('should transform assemblyOptions', () => {
       const parentPrice = 450
       const parentQuantity = 1
 
       const resultBell = transformAssemblyOptions({
-        assemblyOptionsItems: customBell.items as any,
+        assemblyOptionsItems: customBell.items,
         inputValues: {},
         parentPrice,
         parentQuantity,
       })
 
       expect(resultBell.options).toHaveLength(5)
-
       const addonOption = resultBell.options[0] as ItemOption
       expect(addonOption.assemblyId).toBe('add-on_Add-on')
       expect(addonOption.id).toBe('2000588')
@@ -106,7 +105,6 @@ describe('assemblyOptions module', () => {
       })
 
       expect(resultPizza.options).toHaveLength(2)
-
       const pizzaOption = resultPizza.options[0] as ItemOption
       expect(pizzaOption.assemblyId).toBe('pizza_composition_Pizza flavor')
       expect(pizzaOption.id).toBe('5101')
@@ -118,7 +116,7 @@ describe('assemblyOptions module', () => {
       expect(drinksOptions.options).toBeUndefined()
     })
 
-    it('should correctly transform assembly options with input values', () => {
+    it('input values', () => {
       const parentPrice = 450
       const parentQuantity = 1
 
@@ -130,8 +128,7 @@ describe('assemblyOptions module', () => {
       })
 
       expect(resultStar.options).toHaveLength(1)
-
-      const [customization] = resultStar.options
+      const customization = resultStar.options[0] as ItemOption
       expect(customization.assemblyId).toBe('Customization')
       expect(customization.inputValues).toMatchObject({
         Font: 'Sans serif',
@@ -139,6 +136,154 @@ describe('assemblyOptions module', () => {
         'Back text': 'Verso',
         'Glossy print': true,
       })
+      expect(resultStar).toMatchInlineSnapshot(`
+        Object {
+          "assemblyOptions": Object {
+            "added": Array [],
+            "parentPrice": 450,
+            "removed": Array [],
+          },
+          "options": Array [
+            Object {
+              "assemblyId": "Customization",
+              "inputValues": Object {
+                "Back text": "Verso",
+                "Font": "Sans serif",
+                "Front text": "Frente",
+                "Glossy print": true,
+              },
+            },
+          ],
+        }
+      `)
+    })
+
+    it('recursive input values', () => {
+      const parentPrice = 450
+      const parentQuantity = 1
+
+      const resultBell = transformAssemblyOptions({
+        assemblyOptionsItems: customBell.items,
+        inputValues: customBell.inputValues,
+        parentPrice,
+        parentQuantity,
+      })
+
+      expect(resultBell.options).toHaveLength(5)
+
+      const engraving = resultBell.options[4] as ItemOption
+      expect(engraving.options).toHaveLength(1)
+
+      const recursiveInputValue = engraving.options![0] as ItemOption
+      expect(recursiveInputValue.assemblyId).toBe('1-3-lines')
+      expect(recursiveInputValue.inputValues).toBe(
+        customBell.inputValues['1-3-lines']
+      )
+      expect(resultBell).toMatchInlineSnapshot(`
+        Object {
+          "assemblyOptions": Object {
+            "added": Array [
+              Object {
+                "choiceType": "TOGGLE",
+                "extraQuantity": 1,
+                "item": Object {
+                  "id": "2000588",
+                  "name": "Bells add-ons Logo small",
+                  "quantity": 1,
+                  "sellingPrice": 75,
+                  "sellingPriceWithAssemblies": 75,
+                },
+                "normalizedQuantity": 1,
+              },
+              Object {
+                "choiceType": "TOGGLE",
+                "extraQuantity": 1,
+                "item": Object {
+                  "id": "2000589",
+                  "name": "Bells add-ons Logo big",
+                  "quantity": 1,
+                  "sellingPrice": 90,
+                  "sellingPriceWithAssemblies": 90,
+                },
+                "normalizedQuantity": 1,
+              },
+              Object {
+                "choiceType": "SINGLE",
+                "extraQuantity": 1,
+                "item": Object {
+                  "id": "2000592",
+                  "name": "Bells add-ons Script",
+                  "quantity": 1,
+                  "sellingPrice": 15,
+                  "sellingPriceWithAssemblies": 15,
+                },
+                "normalizedQuantity": 1,
+              },
+              Object {
+                "choiceType": "SINGLE",
+                "extraQuantity": 1,
+                "item": Object {
+                  "assemblyOptions": Object {
+                    "added": Array [],
+                    "parentPrice": 26,
+                    "removed": Array [],
+                  },
+                  "id": "2000586",
+                  "name": "Bells add-ons 1-3 lines",
+                  "quantity": 1,
+                  "sellingPrice": 26,
+                  "sellingPriceWithAssemblies": 26,
+                },
+                "normalizedQuantity": 1,
+              },
+            ],
+            "parentPrice": 450,
+            "removed": Array [],
+          },
+          "options": Array [
+            Object {
+              "assemblyId": "add-on_Add-on",
+              "id": "2000588",
+              "quantity": 1,
+              "seller": "1",
+            },
+            Object {
+              "assemblyId": "add-on_Add-on",
+              "id": "2000589",
+              "quantity": 1,
+              "seller": "1",
+            },
+            Object {
+              "assemblyId": "text_style_Text Style",
+              "id": "2000591",
+              "quantity": 0,
+              "seller": "1",
+            },
+            Object {
+              "assemblyId": "text_style_Text Style",
+              "id": "2000592",
+              "quantity": 1,
+              "seller": "1",
+            },
+            Object {
+              "assemblyId": "engraving_Engraving",
+              "id": "2000586",
+              "options": Array [
+                Object {
+                  "assemblyId": "1-3-lines",
+                  "inputValues": Object {
+                    "Line 1": "First line",
+                    "Line 2": "Second line",
+                    "Line 3": "Third line",
+                  },
+                },
+              ],
+              "quantity": 1,
+              "seller": "1",
+            },
+          ],
+        }
+      `)
     })
 
     it('empty input values should result in empty options', () => {
