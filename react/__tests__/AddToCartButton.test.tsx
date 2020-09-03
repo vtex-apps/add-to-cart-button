460b458b1799a2ecab4603ec3ba602a22d636d28
Lucas Pacheco <x2_HvwAsje-q9Fqnm1gV>
2020-09-03 11:26:21 -0300

    added new mock data

--- react/__tests__/AddToCartButton.test.tsx
+++ react/__tests__/AddToCartButton.test.tsx
@@ -307,6 +307,8 @@ describe('AddToCartButton component', () => {
           imageUrl:
             'https://storecomponents.vtexassets.com/arquivos/ids/155518/download--40-.png?v=636942495289870000',
           referenceId: 'red star',
+          sellerName: 'Kawasaki',
+          seller: "1",
         },
         {
           skuId: '2000535',
@@ -322,6 +324,8 @@ describe('AddToCartButton component', () => {
           imageUrl:
             'https://storecomponents.vtexassets.com/arquivos/ids/155488-500-auto?width=500&height=auto&aspect=true',
           referenceId: undefined,
+          sellerName: 'Samsung',
+          seller: "2",
         },
       ],
     }
