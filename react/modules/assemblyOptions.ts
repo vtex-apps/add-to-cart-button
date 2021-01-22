c5340726ea953596ce9f293dc32d920295a4ac3f
igorbrasileiro <brasileiro456@gmail.com>
2021-01-22 14:44:12 -0300

    Parse assemblyOptions id

--- react/modules/assemblyOptions.ts
+++ react/modules/assemblyOptions.ts
@@ -79,6 +79,61 @@ export function sumAssembliesPrice(
   }, 0)
 }
 
+function getObjectIdByPath({
+  path,
+  object,
+}: {
+  path: string[] | string
+  object: any
+}) {
+  if (Array.isArray(path)) {
+    return path.reduce(
+      (result, pathKey) => (result ? result[pathKey] : undefined),
+      object
+    )
+  }
+
+  return object[path]
+}
+
+function parseObjectIdToRemoveDollar({
+  path,
+  object,
+}: {
+  path: string[] | string
+  object: any
+}) {
+  const parsed = getObjectIdByPath({ path, object })
+
+  const splitedId = parsed?.split('$$$')
+
+  return splitedId[splitedId.length - 1]
+}
+
+function formatAddedAssembliesId(added: CartAddedOption[]) {
+  added.forEach(option => {
+    option.item.id = parseObjectIdToRemoveDollar({
+      path: ['item', 'id'],
+      object: option,
+    })
+  })
+}
+
+function formatOptionsIds(options: ItemOption[]) {
+  options.forEach(option => {
+    if (option.assemblyId) {
+      option.assemblyId = parseObjectIdToRemoveDollar({
+        path: 'assemblyId',
+        object: option,
+      })
+    }
+
+    if (option.id) {
+      option.id = parseObjectIdToRemoveDollar({ path: 'id', object: option })
+    }
+  })
+}
+
 export function transformAssemblyOptions({
   assemblyOptionsItems = {},
   inputValues = {},
@@ -185,6 +240,13 @@ export function transformAssemblyOptions({
       })
     }
   }
+
+  /*
+    parse ids to remove $$$ that is generated at product-customizer (assembly options).
+    Look useAssemblyOptions, function generateId, at line 18.
+  */
+  formatOptionsIds(options)
+  formatAddedAssembliesId(added)
 
   return {
     options,
