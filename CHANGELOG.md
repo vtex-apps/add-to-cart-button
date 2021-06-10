a3739f67fea11eebb719d7a48df414e77acc059e
Thalyta Fabrine <thalyta.trindade@vtex.com.br>
2021-06-10 11:49:31 -0300

    Revert "Merge pull request #73 from vtex-apps/fix/autocomplete-on-touch"
    This reverts commit 54ac656c21602bc9687507b6ac43341a36260103, reversing
    changes made to ad605783b3683b5145209a33c2ece010cdf7042f.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,9 +7,6 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
-### Fixed
-- Add to cart from mobile autocomplete.
-
 ## [0.26.0] - 2021-05-05
 
 ### Added 
--- react/AddToCartButton.tsx
+++ react/AddToCartButton.tsx
@@ -285,20 +285,12 @@ function AddToCartButton(props: Props) {
     </span>
   )
 
-  const touchDevice =
-    'ontouchstart' in window ||
-    (typeof navigator !== 'undefined' &&
-      (navigator?.maxTouchPoints || navigator?.msMaxTouchPoints))
-
   const ButtonWithLabel = (
     <Button
       block
       isLoading={isFakeLoading}
       disabled={disabled || !available}
-      onClick={!touchDevice && handleClick}
-      // onTouchEnd is necessary because when using the button on mobile (with touch)
-      // the `preventDefault` is not mapped correctly in `onClick` and closes the autocomplete
-      onTouchEnd={touchDevice && handleClick}
+      onClick={handleClick}
     >
       {available ? availableButtonContent : unavailableButtonContent}
     </Button>
